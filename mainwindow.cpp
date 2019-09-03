2e798ea16d55c45a33435dc704797ac4e746b742
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-09-03 20:18:04 -0300

    First Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QString>
+#include <QFileDialog>
+#include <QFile>
+#include <iostream>
+#include <QTextBrowser>
+#include <QInputDialog>
+#include <QIODevice>
+#include <QTextStream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_bt_new_clicked()
+{
+    if (file)
+        return;
+   QString fileName = QInputDialog::getText(this, "File name", "Name to be saved");
+   ui->tb_status->setText(fileName);
+   file = new QFile(fileName);
+   file->open(QIODevice::ReadWrite | QIODevice::Text);
+}
+
+void MainWindow::on_bt_open_clicked()
+{
+    if (file)
+        return;
+    QString fileName = QFileDialog::getOpenFileName(this,"Open Tsoxic File", "", "Tsoxic Files (*.tsx)");
+    ui->tb_status->setText(fileName);
+    file = new QFile(fileName);
+    if (!file->open(QIODevice::ReadWrite | QIODevice::Text))
+        return;
+
+    while (!file->atEnd()) {
+        QString buf = file->readLine();
+        buf[buf.size() - 1] = '\0';
+        ui->te_code_area->append(buf);
+    }
+}
+
+void MainWindow::on_bt_save_clicked()
+{
+    if (!file)
+        return;
+
+    QTextStream out(file);
+    out << ui->te_code_area->toPlainText();
+}
---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_bt_new_clicked();
+
+    void on_bt_open_clicked();
+
+    void on_bt_save_clicked();
+
+private:
+    QFile *file = nullptr;
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
